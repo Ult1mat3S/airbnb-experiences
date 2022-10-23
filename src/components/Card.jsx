e8da5815ca0f2ed28162b161df10fd67540b2df9
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-10-23 04:03:17 -0600

    fix: add classnames to fix styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,3 @@
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
 import katieZaferes from "./../assets/katieZaferes.svg";
 import star from "./../assets/star1.svg";
 
@@ -27,11 +9,11 @@ export default function Card() {
         <div className="card-stats">
           <img src={star} alt="" className="card-star" />
           <span>5.0</span>
-          <span> (6) • </span>
-          <span>USA</span>
+          <span className="gray"> (6) • </span>
+          <span className="gray">USA</span>
           <p className="card-title">Life lessons with Katie Zaferes</p>
           <p className="card-price">
-            <span>From $136</span> / person
+            <span className="bold">From $136</span> / person
           </p>
         </div>
       </section>
