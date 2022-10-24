e28abf01330f02c6ae1de1de890b9fbf07c39a67
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-10-24 04:17:08 -0600

    feat: use react props, optimize images and remove dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import katieZaferes from "./assets/katieZaferes.svg";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card
+        img={katieZaferes}
+        rating="5.0"
+        location="USA"
+        name="Katie Zaferes"
+        price="136"
+      />
       <Card />
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import katieZaferes from "./../assets/katieZaferes.svg";
 import star from "./../assets/star1.svg";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <>
       <section className="card">
-        <img src={katieZaferes} alt="" className="card-photo" />
+        <img src={props.img} alt="" className="card-photo" />
         <div className="card-stats">
           <img src={star} alt="" className="card-star" />
-          <span>5.0</span>
+          <span>{props.rating}</span>
           <span className="gray"> (6) • </span>
-          <span className="gray">USA</span>
-          <p className="card-title">Life lessons with Katie Zaferes</p>
+          <span className="gray">{props.location}</span>
+          <p className="card-title">Life lessons with {props.name}</p>
           <p className="card-price">
-            <span className="bold">From $136</span> / person
+            <span className="bold">From ${props.price}</span> / person
           </p>
         </div>
       </section>
