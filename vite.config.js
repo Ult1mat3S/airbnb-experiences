b1b8861ce5f61b090eee592461b0f41e7b69469b
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-10-31 04:38:08 -0600

    fix: github page build to separate folders

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,24 @@ export default defineConfig({
 
   build: {
     emptyOutDir: true,
-    outDir: "./dist",
     rollupOptions: {
       output: {
-        entryFileNames: `[name]` + hash + `.js`,
-        chunkFileNames: `[name]` + hash + `.js`,
-        assetFileNames: `[name]` + hash + `.[ext]`,
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
+            return "assets/images/[name]-[hash][extname]";
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return "assets/[name]-[hash][extname]";
+        },
       },
     },
   },
