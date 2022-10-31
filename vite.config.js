a1fee37283cec3e764e02ad2287a6693b33f8fbb
Ult1mat3S <52617311+Ult1mat3S@users.noreply.github.com>
2022-10-31 03:38:17 -0600

    feat: add hash util

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
+import { hash } from "./src/utils/functions.jsx";
 
 export default defineConfig({
   base: "/airbnb-experiences/",
@@ -8,4 +9,13 @@ export default defineConfig({
     open: true,
   },
   plugins: [react(), svgrPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`,
+      },
+    },
+  },
 });
